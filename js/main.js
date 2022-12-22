f71d0ce75b673cae43d32037648a2ea822772c1b
klekovvlad <you@example.com>
2022-12-22 10:28:44 +0300

    add menu and more fixed

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-let generated = [];
+const appMain = document.querySelector('.app');
 let images = [
     './img/1.png',
     './img/2.png',
@@ -9,99 +9,147 @@ let images = [
     './img/7.png',
     './img/8.png',
 ];
+let generated = [];
+let game = [];
+let point = 0;
 const cardsLength = 16;
-let min = 0;
-let max = 7;
-const appMain = document.querySelector('.app');
-const cardsItem = document.createElement('div');
-cardsItem.classList.add('cards');
-appMain.prepend(cardsItem);
-let x = 1;
-let i = 1;
+timer = 60;
 const reload = document.createElement('a');
 reload.classList.add('reload');
+reload.innerHTML = 'Начать заново';
 reload.href = '/';
+startMenu();
 
-const timerItem = document.createElement('div');
-timerItem.classList.add('timer');
-timer = 60;
-timerItem.innerHTML = timer;
-function timerUpdate() {
-    if (timer === 1) {
-        clearInterval(time);
-        loose();
-    }
-    timer--
-    timerItem.innerHTML = timer;
-}
-time = setInterval(timerUpdate, 1000);
-
-while(i <= cardsLength) {
-    let random = Math.floor(Math.random() * (max - min) + min);
-    const card = document.createElement('div');
-    card.classList.add('card');
-    const cardNum = document.createElement('img');
-    cardNum.classList.add('num');
-    cardsItem.append(card);
-    card.append(cardNum);
-    if(generated.length <= (cardsLength - 2)) {
-        while(x <= 2) {
-            let image = images[random];
-            generated.push({random, image});
-            x++;
-        }
-        x = 1;
-    }
-    i++;
-};
-
-let point = 0;
-const pointItem = document.createElement('div');
-pointItem.classList.add('point');
-pointItem.innerHTML = 'Количество очков:' + '' + point;
-appMain.prepend(pointItem, timerItem);
-
-const card = document.querySelectorAll('.card');
-const num = document.querySelectorAll('.num');
-function shuffle(array) {
-    array.sort(() => Math.random() - 0.5);
-};
-let game = [];
-
-shuffle(generated); 
-shuffle(generated);
-card.forEach(function(el, index) {
-    num[index].src = generated[index].image;
-    el.addEventListener('click', () => {
-        el.classList.add('card-animation');
-        el.classList.add('card-active');
-        num[index].classList.add('num-active');
-        let number = generated[index].image;
-        game.push({number, index});
-        if(game.length === 2) {
-            hideCard();
-            setTimeout(showCard, 1000);
-            setTimeout(checkCard, 1000);
-        };
+function startMenu() {
+    const start = document.createElement('div');
+    start.classList.add('start');
+    const startHeader = document.createElement('h1');
+    startHeader.classList.add('start-header');
+    startHeader.innerHTML = 'Найди пару'
+    const startButton = document.createElement('button');
+    startButton.classList.add('start-button');
+    startButton.innerHTML = 'Новая игра';
+    start.append(startHeader, startButton);
+    appMain.append(start);
+    startButton.addEventListener('click', () => {
+        checkEasy();
     });
-});
+};
+
+function checkEasy() {
+    const start = document.querySelector('.start');
+    const startHeader = document.querySelector('.start-header');
+    const startButton = document.querySelector('.start-button');
+    const startButtonHard = document.createElement('button');
+    startButtonHard.classList.add('start-button');
+    startButton.innerHTML = 'Легкая';
+    startButtonHard.innerHTML = 'Сложная';
+    startHeader.innerHTML = 'Выбери уровень сложности';
+    start.append(startButtonHard);
+    startButton.addEventListener('click', () => {
+        startGame();
+    });
+    startButtonHard.addEventListener('click', () => {
+        timer = 30;
+        startGame();
+    })
+}
+
+function startTimer() {
+    const timerItem = document.createElement('div');
+    timerItem.classList.add('timer');
+    timerItem.innerHTML = timer;
+    function timerUpdate() {
+        if (timer === 1) {
+            clearInterval(time);
+            loose();
+        }
+        timer--
+        timerItem.innerHTML = timer;
+    }
+    time = setInterval(timerUpdate, 1000);
+    appMain.prepend(timerItem);
+}
+
+function startGame() {
+    appMain.innerHTML = '';
+    startTimer();
+    let min = 0;
+    let max = 7;
+    const cardsItem = document.createElement('div');
+    cardsItem.classList.add('cards');
+    appMain.prepend(cardsItem);
+    let x = 1;
+    let i = 1;
+    while(i <= cardsLength) {
+        let random = Math.floor(Math.random() * (max - min) + min);
+        const card = document.createElement('div');
+        card.classList.add('card');
+        const cardNum = document.createElement('img');
+        cardNum.classList.add('num');
+        cardsItem.append(card);
+        card.append(cardNum);
+        if(generated.length <= (cardsLength - 2)) {
+            while(x <= 2) {
+                let image = images[random];
+                generated.push({random, image});
+                x++;
+            }
+            x = 1;
+        }
+        i++;
+    };
+    const pointItem = document.createElement('div');
+    pointItem.classList.add('point');
+    pointItem.innerHTML = 'Количество очков:' + '' + point;
+    appMain.prepend(pointItem);
+    const card = document.querySelectorAll('.card');
+    const num = document.querySelectorAll('.num');
+    function shuffle(array) {
+        array.sort(() => Math.random() - 0.5);
+    };
+    shuffle(generated); 
+    shuffle(generated);
+    card.forEach(function(el, index) {
+        num[index].src = generated[index].image;
+        el.addEventListener('click', () => {
+            el.classList.add('card-animation');
+            el.classList.add('card-active');
+            num[index].classList.add('num-active');
+            let number = generated[index].image;
+            game.push({number, index});
+            if(game.length === 2) {
+                hideCard();
+                setTimeout(showCard, 1000);
+                setTimeout(checkCard, 1000);
+            };
+        });
+    });
+};
 
 function hideCard() {
+    const card = document.querySelectorAll('.card');
     for(let cardIndex = 0; cardIndex < card.length; cardIndex++) {
         card[cardIndex].classList.add('card-active');
     };
 };
 function showCard() {
+    const card = document.querySelectorAll('.card');
     for(let cardIndex = 0; cardIndex < card.length; cardIndex++) {
         card[cardIndex].classList.remove('card-active');
     };
 };
 function checkCard() {
+    const card = document.querySelectorAll('.card');
+    const num = document.querySelectorAll('.num');
+    const pointItem = document.querySelector('.point');
     if(game[0].number === game[1].number) {
         point++;
         pointItem.innerHTML = 'Количество очков:' + ' ' + point;
         game = [];
+        console.log(point)
         if (point >= (cardsLength / 2)) {
+            clearInterval(time);
             winner();
         }
     }else{
